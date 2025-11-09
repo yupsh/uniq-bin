6fb6109eb0342a656dedc95b8be34165a87452f1
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:20 -0800

    tests

--- yup-uniq/main.go
+++ yup-uniq/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/uniq"
 )
 
@@ -73,7 +73,7 @@ func action(c *cli.Context) error {
 
 	// Add file arguments (or none for stdin)
 	for i := 0; i < c.NArg(); i++ {
-		params = append(params, yup.File(c.Args().Get(i)))
+		params = append(params, gloo.File(c.Args().Get(i)))
 	}
 
 	// Add flags based on CLI options
@@ -98,5 +98,5 @@ func action(c *cli.Context) error {
 
 	// Create and execute the uniq command
 	cmd := Uniq(params...)
-	return yup.Run(cmd)
+	return gloo.Run(cmd)
 }
